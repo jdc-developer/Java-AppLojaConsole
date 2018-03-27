6790b1b9ceb074e1119a93b971a6872c95f9fe77
Jorge Do Carmo <jorge.do.carmo123@gmail.com>
2018-03-26 23:40:25 -0300

    Adicionado Javadoc

---
+++ src/main/java/jdc/loja/view/ConsoleView.java
@@ -0,0 +1,10 @@
+package jdc.loja.view;
+
+/**
+ * Aplicativo simples feito no console do Java simulando uma loja utilizando gerência de dados com objetos serializados
+ * @author Jorge Do Carmo
+ * @version 1.0
+ */
+public class ConsoleView {
+
+}
