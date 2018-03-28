4918ed7e5d5f72d486d871384a87248e67aa41fe
Jorge Do Carmo <jorge.do.carmo123@gmail.com>
2018-03-28 00:45:34 -0300

    Novo tratamento de persistencia

--- src/main/java/jdc/loja/dao/impl/GenericDAOImpl.java
+++ src/main/java/jdc/loja/dao/impl/GenericDAOImpl.java
@@ -1,7 +1,13 @@
 package jdc.loja.dao.impl;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
 import java.util.List;
 
 import jdc.loja.dao.GenericDAO;
@@ -15,18 +21,44 @@ import jdc.loja.exception.Excecao;
  * @param <K> Tipo de dado da chave primaria
  * @see GenericDAO
  */
-public class GenericDAOImpl<C, K> implements GenericDAO<C, K>{
+public abstract class GenericDAOImpl<C, K> implements GenericDAO<C, K>{
 
+	private static final String persistence = System.getProperty("user.dir") + "/app-resources/";
 	protected ObjectOutputStream output;
+	private BufferedReader buffReader;
+	private String pasta;
 	
-	public GenericDAOImpl(ObjectOutputStream output) {
-		this.output = output;
+	public GenericDAOImpl(String pasta) throws Excecao {
+		String caminho = persistence + pasta + "sequence.txt";
+		FileReader reader = null;
+		try {
+			try {
+				reader = new FileReader(caminho);
+			} catch (FileNotFoundException e) {
+				FileWriter writer = new FileWriter(caminho);
+				PrintWriter dados = new PrintWriter(writer);
+				dados.println(1);
+				dados.close();
+				writer.close();
+				reader = new FileReader(caminho);
+			}
+			finally {
+				buffReader = new BufferedReader(reader);
+				this.pasta = pasta;
+				reader.close();
+			}
+		} catch (Exception e1) {
+			e1.printStackTrace();
+			throw new Excecao("Erro ao criar diretório de arquivos");
+		}
 	}
 	
 	public void cadastrar(C bean) throws Excecao {
 		try {
+			output = new ObjectOutputStream(new FileOutputStream(persistence + pasta + bean.getClass().toString() + buffReader.readLine()));
+			bean.getClass().getDeclaredMethod("setCodigo", bean.getClass());
 			output.writeObject(bean);
-		} catch (IOException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 			throw new Excecao("Erro ao cadastrar");
 		}
