319f11d873f1310f429443f8f44718eb4448c841
Jorge Do Carmo <jorge.do.carmo123@gmail.com>
2018-04-13 02:08:17 -0300

    Otimização do console

---
+++ src/main/java/jdc/loja/util/ConsoleUtil.java
@@ -0,0 +1,169 @@
+package jdc.loja.util;
+
+import java.lang.reflect.Method;
+import java.util.Scanner;
+
+import jdc.loja.exception.Excecao;
+
+/**
+ * Classe para armazenar os métodos genéricos do console
+ * @author Jorge Do Carmo
+ *
+ */
+public class ConsoleUtil {
+
+	private static Scanner entrada = new Scanner(System.in);
+	
+	/**
+	 * Método genérico para chamar outro método de acordo com o comando enviado
+	 * @param action
+	 */
+	public static void callAction(int action) {
+		try {
+			Class<?> classe = Class.forName("jdc.loja.view.ConsoleView");
+			Method metodo = classe.getMethod("acao" + action);
+			metodo.invoke(classe);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+	}
+	
+	/**
+	 * Método genérico para receber comandos e validá-los de acordo com o tipo de dado e as opções válidas
+	 * @param validos
+	 * @return valor do comando
+	 * @throws Excecao
+	 */
+	public static int comando(int[] validos) throws Excecao {
+		boolean valido = false;
+		String input = entrada.next();
+		StringBuilder processedChars = new StringBuilder();
+		
+		if(input.isEmpty()) {
+			throw new Excecao("Comando inválido, digite novamente");
+		}
+		
+		for(int i=0 ; i < input.length() ; i++){
+		    char c = input.charAt(i);
+		    if(!Character.isDigit(c)){
+		    	throw new Excecao("Comando inválido, digite novamente");
+		    }else{
+		        processedChars.append(c);
+		    }
+		}
+		
+		int comando = Integer.parseInt(processedChars.toString());
+		
+		for(int i = 0; i < validos.length; i++) {
+			if(validos[i] == comando) {
+				valido = true;
+			}
+		}
+		
+		if(valido) {
+			return comando;
+		}
+		else {
+			throw new Excecao("Comando inválido, digite novamente");
+		}
+	}
+	
+	public static String receberString() throws Excecao{
+		boolean valido = false;
+		String input = entrada.next();
+		StringBuilder processedChars = new StringBuilder();
+		
+		if(input.isEmpty()) {
+			throw new Excecao("Valor inválido, digite novamente");
+		}
+		
+		for(int i=0 ; i<input.length() ; i++){
+		    char c = input.charAt(i);
+		    if(Character.isLetter(c)){
+		    	valido = true;
+		    }
+		    processedChars.append(c);
+		}
+		
+		if(valido) {
+			return processedChars.toString();
+		}
+		else {
+			throw new Excecao("Valor inválido, digite novamente");
+		}
+	}
+	
+	public static float receberFloat() throws Excecao{
+		String input = entrada.next();
+		StringBuilder processedChars = new StringBuilder();
+		
+		if(input.isEmpty()) {
+			throw new Excecao("Valor inválido, digite novamente");
+		}
+		
+		for(int i=0 ; i<input.length() ; i++){
+		    char c = input.charAt(i);
+		    if(!Character.isDigit(c)){
+		    	throw new Excecao("Valor inválido, digite novamente");
+		    }else{
+		        processedChars.append(c);
+		    }
+		}
+		
+		float valor = Float.parseFloat(processedChars.toString());
+		
+		return valor;
+	}
+	
+	public static void confirmarAcao(String titulo, String[] valores, int retorno) {
+		System.out.println(titulo);
+		
+		for(int i = 0; i < valores.length; i++) {
+			System.out.println(valores[i]);
+		}
+		
+		System.out.println("\n1. Confirmar / 2. Cancelar");
+		
+		int comando = 0;
+		int validos[] = new int[] {1, 2};
+		
+		try {
+			comando = comando(validos);
+		} catch (Excecao e) {
+			confirmarAcao(titulo, valores, retorno);
+		}
+		
+		if(comando == 2) {
+			System.out.println("Cancelado");
+			callAction(retorno);
+		}
+	}
+	
+	public static String propriedadeString(String propriedade) {
+		System.out.println("\n" + propriedade);
+		
+		String valor = null;
+		
+		try {
+			valor = receberString();
+		} catch (Excecao e) {
+			propriedadeString(propriedade);
+		}
+		
+		return valor;
+	}
+	
+	public static float propriedadeFloat(String propriedade) {
+		System.out.println("\n" + propriedade);
+		
+		float valor = 0;
+		
+		try {
+			valor = receberFloat();
+		} catch (Excecao e) {
+			propriedadeFloat(propriedade);
+		}
+		
+		return valor;
+	}
+}
--- src/main/java/jdc/loja/view/ConsoleView.java
+++ src/main/java/jdc/loja/view/ConsoleView.java
@@ -1,14 +1,12 @@
 package jdc.loja.view;
 
-import java.lang.reflect.Method;
-import java.util.Scanner;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import jdc.loja.beans.ProdutoBean;
 import jdc.loja.bo.ProdutoBO;
 import jdc.loja.exception.Excecao;
+import jdc.loja.util.ConsoleUtil;
 
 /**
  * Aplicativo simples feito no console do Java simulando uma loja utilizando gerência de dados com objetos serializados
@@ -18,21 +16,19 @@ import jdc.loja.exception.Excecao;
 public class ConsoleView {
 	
 	private static final Logger log = LoggerFactory.getLogger(ConsoleView.class);
-	private static Scanner entrada;
 
 	/**
 	 * O main apresenta a aplicação e chama a primeira ação. Todo o fluxo está dividido em ações para possibilitar redirecionamento
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		entrada = new Scanner(System.in);
 		log.info("Iniciando aplicação\n");
 		
 		System.out.println("--------------------------- BEM-VINDO AO JAVA-APP-LOJA ---------------------------");
 		System.out.println("------------------------ Desenvolvido por Jorge Do Carmo -------------------------");
 		System.out.println("----------------------------- Versão 1.0 / 2018 ----------------------------------");
 		
-		callAction(0);
+		ConsoleUtil.callAction(0);
 	}
 	
 	public static void acao0() {
@@ -44,13 +40,13 @@ public class ConsoleView {
 		int comando = 0;
 		
 		try {
-			comando = comando(validos);
+			comando = ConsoleUtil.comando(validos);
 		} catch (Excecao e) {
-			callAction(0);
+			ConsoleUtil.callAction(0);
 		}
 		
 		if(comando != 0) {
-			callAction(comando);
+			ConsoleUtil.callAction(comando);
 		}
 	}
 	
@@ -68,15 +64,15 @@ public class ConsoleView {
 			
 			int[] validos = new int[] {1, 2, 3, 9};
 			
-			comando = comando(validos);
+			comando = ConsoleUtil.comando(validos);
 		} catch (Excecao e) {
-			callAction(1);
+			ConsoleUtil.callAction(1);
 		}
 		
 		if(comando != 0 && comando != 9) {
-			callAction(comando + 10);
+			ConsoleUtil.callAction(comando + 10);
 		} else if (comando == 9) {
-			callAction(0);
+			ConsoleUtil.callAction(0);
 		}
 	}
 	
@@ -84,135 +80,27 @@ public class ConsoleView {
 		
 		System.out.println("--------------------------- CADASTRAR PRODUTO ---------------------------");
 		System.out.println("\nDIGITE OS DADOS DO PRODUTO:");
-		System.out.println("Descrição:");
 		
 		String descricao = null;
-		float valor = 0;
+		float preco = 0;
 		
-		try {
-			descricao = receberString();
-		} catch (Excecao e) {
-			callAction(11);
-		}
+		descricao = ConsoleUtil.propriedadeString("Descrição");
 		
 		System.out.println("\nDescrição salva");
-		System.out.println("Valor:");
 		
-		try {
-			valor = receberFloat();
-		} catch (Excecao e) {
-			callAction(11);
-		}
+		preco = ConsoleUtil.propriedadeFloat("Preço:");
 		
-		ProdutoBean bean = new ProdutoBean(descricao, valor);
+		String[] valores = new String[] {"\nDescrição: " + descricao, "Preço: " + preco};
+		ConsoleUtil.confirmarAcao("\nCadastrar este produto?", valores, 1);
+		
+		ProdutoBean bean = new ProdutoBean(descricao, preco);
 		
 		try {
 			ProdutoBO.cadastrar(bean);
 		} catch (Excecao e) {
-			e.printStackTrace();
+			ConsoleUtil.callAction(11);
 		} finally {
-			callAction(1);
+			ConsoleUtil.callAction(1);
 		}
-	}
-	
-	/**
-	 * Método genérico para chamar outro método de acordo com o comando enviado
-	 * @param action
-	 */
-	public static void callAction(int action) {
-		try {
-			Class<?> classe = Class.forName("jdc.loja.view.ConsoleView");
-			Method metodo = classe.getMethod("acao" + action);
-			metodo.invoke(classe);
-		} catch (Exception e) {
-			e.printStackTrace();
-		} 
-	}
-	
-	/**
-	 * Método genérico para receber comandos e validá-los de acordo com o tipo de dado e as opções válidas
-	 * @param validos
-	 * @return valor do comando
-	 * @throws Excecao
-	 */
-	public static int comando(int[] validos) throws Excecao {
-		boolean valido = false;
-		String input = entrada.next();
-		StringBuilder processedChars = new StringBuilder();
-		
-		if(input.isEmpty()) {
-			throw new Excecao("Comando inválido, digite novamente");
-		}
-		
-		for(int i=0 ; i<input.length() ; i++){
-		    char c = input.charAt(i);
-		    if(!Character.isDigit(c)){
-		    	throw new Excecao("Comando inválido, digite novamente");
-		    }else{
-		        processedChars.append(c);
-		    }
-		}
-		
-		int comando = Integer.parseInt(processedChars.toString());
-		
-		for(int i = 1; i < 10; i++) {
-			if(i == comando) {
-				valido = true;
-			}
-		}
-		
-		if(valido) {
-			return comando;
-		}
-		else {
-			throw new Excecao("Comando inválido, digite novamente");
-		}
-	}
-	
-	public static String receberString() throws Excecao{
-		boolean valido = false;
-		String input = entrada.next();
-		StringBuilder processedChars = new StringBuilder();
-		
-		if(input.isEmpty()) {
-			throw new Excecao("Valor inválido, digite novamente");
-		}
-		
-		for(int i=0 ; i<input.length() ; i++){
-		    char c = input.charAt(i);
-		    if(Character.isLetter(c)){
-		    	valido = true;
-		    }
-		    processedChars.append(c);
-		}
-		
-		if(valido) {
-			return processedChars.toString();
-		}
-		else {
-			throw new Excecao("Valor inválido, digite novamente");
-		}
-	}
-	
-	public static float receberFloat() throws Excecao{
-		String input = entrada.next();
-		StringBuilder processedChars = new StringBuilder();
-		
-		if(input.isEmpty()) {
-			throw new Excecao("Valor inválido, digite novamente");
-		}
-		
-		for(int i=0 ; i<input.length() ; i++){
-		    char c = input.charAt(i);
-		    if(!Character.isDigit(c)){
-		    	throw new Excecao("Valor inválido, digite novamente");
-		    }else{
-		        processedChars.append(c);
-		    }
-		}
-		
-		float valor = Float.parseFloat(processedChars.toString());
-		
-		return valor;
 	}
 }
